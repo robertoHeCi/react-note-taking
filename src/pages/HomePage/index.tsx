5bfe9345950f699a86eabde69ca2214b61809cd3
roberto <roberto.hernandez@one-beyond.com>
2024-11-09 22:03:01 +0100

    feat: create home page

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1>HomePage</h1>
+      </div>
+    )
+}
+
+export default HomePage;
