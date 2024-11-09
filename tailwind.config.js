ee5da4979bec954743217d58fad1099e0156ef3a
roberto <roberto.hernandez@one-beyond.com>
2024-11-09 22:03:23 +0100

    style: adjust styles and update config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'selector',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#EBFAFE', // Example primary color
+        secondary: '#FBBF24', // Example secondary color
+        accent: '#F472B6', // Example accent color
+        background: '#F9FAFB', // Example background color
+        text: '#111827', // Example text color
+        // Add more colors as needed
+      },
+    },
   },
   plugins: [],
 }
