9f40f73a5d0f77e060b55346c7c3458ee96638c2
roberto <roberto.hernandez@one-beyond.com>
2024-11-09 22:01:53 +0100

    feat: create theme toogle component

---
+++ src/components/ThemeToggle/index.tsx
@@ -0,0 +1,16 @@
+import useTheme from "@/hooks/useTheme";
+
+
+const ThemeToggle = () => {
+  const { toggleTheme, themeIcon } = useTheme();
+  
+  return (
+    <div className="flex justify-end p-6">
+      <button className="btn w-10 h-10" onClick={toggleTheme}>
+        <img className="w-full h-full" src={themeIcon} alt="theme" />
+      </button>
+    </div>
+  )
+}
+
+export default ThemeToggle;
