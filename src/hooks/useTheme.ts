91f82a8f86b0a747d674356e687d22160bb90b23
roberto <roberto.hernandez@one-beyond.com>
2024-11-09 22:01:02 +0100

    feat: create use theme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,23 @@
+import darkIcon from '@/assets/icons/dark.svg';
+import lightIcon from '@/assets/icons/light.svg';
+import { useState } from 'react';
+
+const useTheme = () => {
+  const DARK_CLASS = 'dark';
+  const LIGHT_CLASS = 'light';
+
+  const [theme, setTheme] = useState(LIGHT_CLASS);
+
+  const toggleTheme = () => {
+    document.documentElement.classList.toggle(DARK_CLASS);
+    setTheme(theme === DARK_CLASS ? LIGHT_CLASS : DARK_CLASS);
+  }
+
+  const themeIcon = theme === DARK_CLASS ? lightIcon : darkIcon;
+  
+
+  return { toggleTheme, themeIcon };
+  
+}
+
+export default useTheme;
