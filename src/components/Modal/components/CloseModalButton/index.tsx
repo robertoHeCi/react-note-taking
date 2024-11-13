cbad4fdacac6c007508d9a4051d5efc884ab76f9
roberto <roberto.hernandez@one-beyond.com>
2024-11-13 21:43:47 +0100

    feat: create components for modal buttons

---
+++ src/components/Modal/components/CloseModalButton/index.tsx
@@ -0,0 +1,12 @@
+const CloseModalButton = ({ isOpen, toggleModal }: { isOpen: boolean, toggleModal: () => void }) => {
+  return (
+    <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={toggleModal}>
+      <svg className="w-3 h-3" aria-hidden={isOpen ? 'true' : 'false'} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+      </svg>
+      <span className="sr-only">Close modal</span>
+    </button>
+  )
+}
+
+export default CloseModalButton;
---
+++ src/components/Modal/components/ModalHandlerButtons/index.tsx
@@ -0,0 +1,21 @@
+export const ModalHandlerButtons = ({ resetForm }: { resetForm: () => void }) => {
+  return (
+    <>
+      <button
+        data-modal-hide="default-modal"
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
+      <button
+        data-modal-hide="default-modal"
+        type="button"
+        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        onClick={resetForm}
+      >
+        Cancel
+      </button>
+    </>
+  )
+}
