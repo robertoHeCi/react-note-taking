90579d3a2e1151ab2c084e1648ea3188d32345ac
roberto <roberto.hernandez@one-beyond.com>
2024-11-13 21:44:33 +0100

    feat: first version handling text notes

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,9 +1,32 @@
 import { NoteType } from "@/hooks/useNote";
+import TextForm from "./components/TextForm";
+import { useForm } from "react-hook-form";
+import { useApiNotes } from "@/hooks/useApiNotes";
+import CloseModalButton from "./components/CloseModalButton";
+import { ModalHandlerButtons } from "./components/ModalHandlerButtons";
 
 const Modal = ({ isOpen, setIsOpen, noteToDisplay }: { isOpen: boolean, setIsOpen: (isOpen: boolean) => void, noteToDisplay: NoteType | null }) => {
 
+  const { createNote } = useApiNotes();
   const toggleModal = () => {
     setIsOpen(!isOpen);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm<Notes.Types.TextNote>()
+
+  const resetForm = () => {
+    toggleModal();
+    reset();
+  }
+
+  const onSubmit = (data: Notes.Types.TextNote) => {
+    createNote({ ...data, type: noteToDisplay?.type || '' });
+    resetForm()
   };
 
   return (
@@ -14,29 +37,18 @@ const Modal = ({ isOpen, setIsOpen, noteToDisplay }: { isOpen: boolean, setIsOpe
           aria-hidden="true"
           className="overflow-y-auto fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-50"
         >
-          <div className="relative p-4 w-11/12 max-h-full animate-slide-up">
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                  Note type: {noteToDisplay?.type}
-                </h3>
-                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={toggleModal}>
-                  <svg className="w-3 h-3" aria-hidden={isOpen ? 'true' : 'false'} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                  </svg>
-                  <span className="sr-only">Close modal</span>
-                </button>
-              </div>
-              <div className="p-4 md:p-5 space-y-4">
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                  {noteToDisplay?.content}
-                </p>
-              </div>
-              <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button data-modal-hide="default-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                <button data-modal-hide="default-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
-              </div>
-            </div>
+          <div className="relative p- w-11/12 max-h-full animate-slide-up">
+            <form className="relative bg-white rounded-lg shadow dark:bg-gray-700" onSubmit={handleSubmit(onSubmit)}>
+              <section className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <CloseModalButton isOpen={isOpen} toggleModal={toggleModal} />
+              </section>
+              <section className="p-4 md:p-5 space-y-4">
+                {noteToDisplay?.type === 'text' && <TextForm register={register} errors={errors} />}
+              </section>
+              <section className="flex items-end justify-end gap-2 p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <ModalHandlerButtons resetForm={resetForm} />
+              </section>
+            </form>
           </div>
         </div>
       )}
