abf2e48315ad29cbcc952a2e34f13dc69030bd80
roberto <roberto.hernandez@one-beyond.com>
2024-11-14 21:57:11 +0100

    chore: add missing id

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -23,14 +23,14 @@ const HomePage = () => {
       return false;
     }
   };
-  
+
   return (
     <div className="flex flex-col items-center justify-start h-screen pt-5">
       <Toolbar handleClick={handleClick} />
       <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} noteToDisplay={noteToDisplay} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full p-4">
         {notes?.filter(isValidJson)?.map((note: { id: string, body: string }) => (
-          <TextNote key={note.id} body={note.body} />
+          <TextNote key={note.id} id={note.id} body={note.body} />
         ))}
       </div>
     </div>
