db025b4ed82adcc9a50b753061725c7df5fb9645
roberto <roberto.hernandez@one-beyond.com>
2024-11-15 19:56:56 +0100

    style: control the content height

--- src/components/TextNote/index.tsx
+++ src/components/TextNote/index.tsx
@@ -1,12 +1,11 @@
 
 const TextNote = ({ note, onClick }: { note: Notes.Types.TextNote, onClick: (note: Notes.Types.TextNote) => void }) => {
 
-
   return (
     <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4" onClick={() => onClick(note)}>
       <div>
         <h4 className="text-gray-800 dark:text-gray-100 font-bold mb-3 z-index-1">{note.title}</h4>
-        <p className="text-gray-800 dark:text-gray-100 text-sm">{note.content}</p>
+        <p className="text-gray-800 dark:text-gray-100 text-sm overflow-hidden line-clamp-6 md:line-clamp-4 lg:line-clamp-6">{note.content}</p>
       </div>
       <div>
         <div className="flex items-center justify-between text-gray-800 dark:text-gray-100">
