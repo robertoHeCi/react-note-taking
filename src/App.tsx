3886751de16a92a071d4f03c192d9c2f70b73a04
roberto <roberto.hernandez@one-beyond.com>
2024-11-16 19:50:53 +0100

    feat: add form provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from '@/layout/MainLayout';
 import HomePage from '@/pages/HomePage';
+import { FormProvider, useForm } from 'react-hook-form';
 const App = () => {
+  const methods = useForm();
   return (
+    <FormProvider {...methods}>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
         </Route>
       </Routes>
+    </FormProvider> 
   );
 }
 
