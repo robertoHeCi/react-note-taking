a1b9785aced1f03ff8e31892c7f5a0712779c366
roberto <roberto.hernandez@one-beyond.com>
2024-11-16 14:04:18 +0100

    test: update handlers and delete unused env

--- src/tests/mocks/handlers.ts
+++ src/tests/mocks/handlers.ts
@@ -1,15 +1,17 @@
 import { http, HttpResponse } from 'msw';
 
+console.log(import.meta.env.VITE_API_URL, 'api');
+
 export const handlers = [
   http.get(`${import.meta.env.VITE_API_URL}/notes`, () => {
     return HttpResponse.json([
-      { id: '1', body: 'Test note 1' },
-      { id: '2', body: 'Test note 2' },
+      { id: 1, body: JSON.stringify({ title: 'Test note 1', content: 'Test note 1', updatedAt: '2024-11-13T00:00:00.000Z', type: 'note' }) },
+      { id: 2, body: JSON.stringify({ title: 'Test note 2', content: 'Test note 2', updatedAt: '2024-11-13T00:00:00.000Z', type: 'note' }) },
     ]);
   }),
   http.get(`${import.meta.env.VITE_API_URL}/notes/:id`, ({ params }) => {
     if (params.id === '1') {  
-      return HttpResponse.json({ id: '1', body: 'Test note 1' });
+      return HttpResponse.json({ id: 1, body: JSON.stringify({ title: 'Test note 1', content: 'Test note 1', updatedAt: '2024-11-13T00:00:00.000Z', type: 'note' }) });
     }
     return HttpResponse.json({ error: 'Note not found' }, { status: 404 });
   }),
