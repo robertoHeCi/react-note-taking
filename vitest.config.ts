fda1326fd36f10c1ac116feb463758bfb1e62319
roberto <roberto.hernandez@one-beyond.com>
2024-11-16 14:04:30 +0100

    test: update test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/tests/setup.ts'],
-    globals: true,
+    environment: "jsdom",
+    env: {
+      VITE_API_URL: "http://test-url.com"
+    },
+    setupFiles: ["./src/tests/setup.ts"],
+    globals: true
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    },
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  server: {
+    headers: {
+      "Service-Worker-Allowed": "/"
+    }
   }
-}); 
+});
