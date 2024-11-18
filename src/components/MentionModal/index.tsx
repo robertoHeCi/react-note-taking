7e4f4a0a21ee016254df91f55b0544af21759ad3
roberto <roberto.hernandez@one-beyond.com>
2024-11-18 20:51:53 +0100

    feat: create mention modal and hook

---
+++ src/components/MentionModal/index.tsx
@@ -0,0 +1,34 @@
+import { useUsers } from '@/hooks/useUsers';
+
+const MentionModal = ({ query, insertMention, showMentions }: { query: string, insertMention: (username: string) => void, showMentions: boolean }) => {
+  const { users, isLoading } = useUsers(showMentions ? query : "melany");
+  return (
+    <div
+      className="absolute z-10 w-64 bg-white dark:bg-gray-800 shadow-lg rounded-lg border border-gray-200 dark:border-gray-700"
+    >
+      {isLoading ? (
+        <div className="p-2 text-sm text-gray-500">Loading...</div>
+      ) : users && users.length > 0 ? (
+        <ul className="py-1">
+          {users.map((user) => (
+            <li
+              key={user.username}
+              className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer flex items-center gap-2"
+              onClick={() => insertMention(user.first_name + " " + user.last_name)}
+              autoFocus
+            >
+              <div>
+                <div className="text-sm font-medium">{user.first_name} {user.last_name}</div>
+                <div className="text-xs text-gray-500">@{user.username}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="p-2 text-sm text-gray-500">No users found</div>
+      )}
+    </div>
+  )
+}
+
+export default MentionModal;
---
+++ src/hooks/useMention.ts
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { useFormContext } from "react-hook-form";
+
+type MentionProps = {
+  contentRef: React.RefObject<HTMLDivElement>;
+};
+
+const useMention = ({ contentRef }: MentionProps) => {
+  const [mentionStartCursorPosition, setMentionStartCursorPosition] =
+    useState<number>(-1);
+  const [mentionQuery, setMentionQuery] = useState<string>("");
+  const [showMentions, setShowMentions] = useState<boolean>(false);
+
+  const { setValue } = useFormContext();  
+
+  const getCaretPosition = () => {
+    const selection = window.getSelection();
+    if (!selection || selection.rangeCount === 0 || !contentRef.current)
+      return -1;
+
+    const range = selection.getRangeAt(0);
+    const preCaretRange = range.cloneRange();
+    const editableDiv = contentRef.current;
+
+    preCaretRange.selectNodeContents(editableDiv);
+    preCaretRange.setEnd(range.endContainer, range.endOffset);
+
+    const position = preCaretRange.toString().length;
+    return position;
+  };
+
+  const onInsertMention = (username: string) => {
+    if (!contentRef.current || !mentionStartCursorPosition) return;
+
+    // Create the mention element
+    const mentionSpan = document.createElement("span");
+    mentionSpan.className =
+      "capitalize dark:bg-red-500 bg-red-200 rounded-md px-1 py-0.5";
+    mentionSpan.contentEditable = "false"; // Make mention non-editable
+    mentionSpan.innerHTML = username;
+
+    const newContent = contentRef.current.innerHTML.replace(
+      `@${mentionQuery}`,
+      mentionSpan.outerHTML
+    );
+    contentRef.current.innerHTML = newContent;
+
+    setShowMentions(false);
+    setValue('content', newContent);
+  };
+
+  const handleOnKeyUp = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "@") {
+      const position = getCaretPosition();
+      setShowMentions(true);
+      setMentionStartCursorPosition(position);
+    } else if (e.key === "Backspace") {
+      const match = contentRef.current?.innerHTML?.match(
+        /(?<!<span[^>]*>.*?)(@(\w*))$/
+      );
+      if (!match) {
+        setShowMentions(false);
+        setMentionQuery("");
+      }
+    } else if (showMentions) {
+      const currentPosition = getCaretPosition();
+      const content = contentRef.current?.textContent || ""; // Using textContent instead of innerHTML
+
+      const queryText = content
+        .slice(mentionStartCursorPosition, currentPosition)
+        .replace(/^\@/, "");
+
+      setMentionQuery(queryText);
+
+      // Hide mentions dropdown if user types a space
+      if (e.key === " " || queryText.includes(" ")) {
+        setShowMentions(false);
+        setMentionQuery("");
+      }
+    }
+  };
+
+  return {
+    handleOnKeyUp,
+    onInsertMention,
+    showMentions,
+    mentionQuery
+  };
+};
+
+export default useMention;
